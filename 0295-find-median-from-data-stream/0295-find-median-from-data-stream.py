e27f0bdb5d1e69a00cda4c59829343a051338d3d
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2023-11-03 19:15:54 -0400

    Time: 455 ms (31.91%), Space: 38.8 MB (8.51%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,41 @@
+class MedianFinder:
+    # add/remove an element from the heap is O(logn) time complexity 
+    # To get the median we want the max value from the small heap whcih is max heap and small value from the max heap which takes O(1) time. 
+
+    def __init__(self):
+    # two heaps, large and small -> minHeap and maxHeap respectively 
+    # heaps should be equal size 
+        self.small, self.large = [], []
+        
+
+    def addNum(self, num: int) -> None:
+        # -1 because python doesn't allow max heap implementation 
+        heapq.heappush(self.small, -1 * num)
+
+        # make sure every num in small is <= every num in large 
+        if(self.small and self.large and 
+        ( -1 * self.small[0]) > self.large[0]):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+
+        # uneven size? 
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        return (-1 * self.small[0] + self.large[0])/2 
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
