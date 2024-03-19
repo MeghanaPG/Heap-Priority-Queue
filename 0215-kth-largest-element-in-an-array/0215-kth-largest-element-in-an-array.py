78190d5227b61f07527097f7a68b46e676078c95
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-03-18 22:44:23 -0400

    Time: 506 ms (20.47%), Space: 29.6 MB (79.21%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -6,6 +6,8 @@ class Solution:
             heapq.heappush(self.maxheap, -1 * num)
         heapq.heapify(self.maxheap)
 
+        # print(self.maxheap)
+        # val = -1 * self.maxheap[k-1]
         for _ in range(k):
             kth_largest = -heapq.heappop(self.maxheap)  # Negate the result to get the actual value
         return kth_largest
