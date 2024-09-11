67ef3da8efdb26700969d911556000037e198436
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-11 08:30:31 -0400

    Time: 41 ms (26.11%), Space: 16.7 MB (25.13%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,23 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # Time complexity: O(2^N)
         res = []
 
-        # array to build subsets 
         subset = []
         def dfs(i):
             if i >= len(nums):
-                # subset is going to be modified, hence we add copies 
                 res.append(subset.copy())
-                return 
-
-            # decision to include nums[i]
+                return res 
+            
             subset.append(nums[i])
             dfs(i+1)
 
-            # decision not to inclusde nums[i]
             subset.pop()
             dfs(i+1)
-
+        
         dfs(0)
         return res 
